3424b3653a837d61b9a35c183c77d357948fb5ac
crazyhaller <suvigyamadrid@gmail.com>
2023-04-27 18:09:53 +0530

    create the about section

---
+++ components/About.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+const About = () => {
+  return (
+    <div className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[##05BFDB]">
+            About
+          </p>
+          <h2 className="py-4">Get to know me!</h2>
+          <p className="py-2 text-gray-600">// I am your normal developer :)</p>
+          <p className="py-2 text-gray-600">
+            Hello and welcome to my portfolio! My name is [Your Name] and I am a
+            beginner MERN stack developer with over 1.5 years of experience. I
+            am passionate about developing high-quality web applications and
+            have worked on multiple projects to improve my skills and gain
+            practical experience. My technical skills include proficiency in
+            JavaScript, React, Node.js, Express and MongoDB. I have a strong
+            foundation in web development concepts such as HTML, CSS, and
+            responsive design. I enjoy learning new technologies and keeping up
+            with the latest trends in the industry.
+          </p>
+          <p className="py-2 text-gray-600">
+            During my journey as a developer, I have worked on several projects
+            ranging from simple web applications to complex e-commerce
+            platforms. I believe that my attention to detail, problem-solving
+            skills, and ability to work collaboratively make me a valuable asset
+            to any team. Thank you for taking the time to visit my portfolio.
+            Please feel free to take a look at my projects and reach out if you
+            have any questions or opportunities to work together.
+          </p>
+          <p className="py-2 text-gray-600 underline cursor-pointer">
+            Check out some of the projects I have worked in!
+          </p>
+        </div>
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <img
+            className="rounded-xl"
+            src="https://images.unsplash.com/photo-1551739440-5dd934d3a94a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
+            alt="/"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
