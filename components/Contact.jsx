9dff154318fff4a8fab0bfd2a57f314997053e22
crazyhaller <suvigyamadrid@gmail.com>
2023-11-11 17:21:14 +0530

    edit contact note color

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -160,8 +160,8 @@ const Contact = () => {
                 <button type="submit" className="w-full p-4 text-blue-800 mt-4">
                   Send Message
                 </button>
-                <label className="uppercase text-sm py-2">
-                  It may take a while for the query to go through please only
+                <label className="uppercase text-sm py-2 mt-2 text-red-500">
+                  It may take a while for the query to go through, please only
                   click the button once.
                 </label>
               </form>
